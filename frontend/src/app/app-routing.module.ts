6bd5457814034338223290bf7160e8813242e88d
NIcoleR1234 <nicolrmn1234@gmail.com>
2023-07-24 14:19:48 -0400

    Select Company basics

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SelectCompanyComponent } from './select-company/select-company.component';
+import { ProjectsComponent } from './projects/projects.component';
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "select", component: SelectCompanyComponent },
+  {path: "projects", component: ProjectsComponent },
  
 ];
 
