d53764cc852725c9bf0e0bcad847126103e168c5
alesscif <alexciogli1@gmail.com>
2023-07-24 10:22:58 -0700

    generated angular components

---
+++ frontend/src/app/components/create-team-overlay/create-team-overlay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-team-overlay',
+  templateUrl: './create-team-overlay.component.html',
+  styleUrls: ['./create-team-overlay.component.css']
+})
+export class CreateTeamOverlayComponent {
+
+}
---
+++ frontend/src/app/components/edit-project-overlay/edit-project-overlay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-project-overlay',
+  templateUrl: './edit-project-overlay.component.html',
+  styleUrls: ['./edit-project-overlay.component.css']
+})
+export class EditProjectOverlayComponent {
+
+}
