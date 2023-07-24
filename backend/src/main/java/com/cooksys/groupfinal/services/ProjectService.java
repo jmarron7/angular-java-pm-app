09274b46ec4d649e895bb3897914f32b180c756a
KieranMueller <kieran98mueller@gmail.com>
2023-07-24 11:22:32 -0500

    Added POST company/{id}/teams/{id}/project , may need refining

--- backend/src/main/java/com/cooksys/groupfinal/services/ProjectService.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/ProjectService.java
@@ -1,5 +1,8 @@
 package com.cooksys.groupfinal.services;
 
+import com.cooksys.groupfinal.dtos.ProjectDto;
+
 public interface ProjectService {
 
+    ProjectDto postProject(Long companyId, Long teamId, ProjectDto projectDto);
 }
