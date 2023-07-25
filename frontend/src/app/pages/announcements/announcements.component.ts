97ca847a4d95ce29f0e9358ba15dee55bf94c19e
KieranMueller <kieran98mueller@gmail.com>
2023-07-25 18:13:04 -0500

    announcement component styling

--- frontend/src/app/pages/announcements/announcements.component.ts
+++ frontend/src/app/pages/announcements/announcements.component.ts
@@ -5,28 +5,29 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
-  styleUrls: ['./announcements.component.css']
+  styleUrls: ['./announcements.component.css'],
 })
 export class AnnouncementsComponent {
-
-  announcements: any[] = [1,2,3,4];
+  announcements: any[] = [1, 2, 3, 4];
   companyId = '';
+  isCreatingPost: boolean = false;
 
-  constructor(private http: HttpClient, private router: Router) { 
+  constructor(private http: HttpClient, private router: Router) {
     let input = this.router.getCurrentNavigation();
     this.companyId = input?.extras?.state?.['companyId'];
     localStorage.setItem('companyId', this.companyId);
   }
 
   ngOnInit() {
-    let url = 'company/' + this.companyId +  'announcements';
+    let url = 'company/' + this.companyId + 'announcements';
     this.http.get<any>(url).subscribe({
-        next: data => {
-            this.announcements = data;
-        },
-        error: error => {
-            console.error(error);
-          }
-      })
+      next: (data) => {
+        console.log(data);
+        this.announcements = data;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 }
