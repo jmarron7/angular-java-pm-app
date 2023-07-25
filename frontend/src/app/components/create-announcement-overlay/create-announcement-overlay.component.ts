81d69f77fc4c63a42f7eca2c8ffab00fd85adcf1
KieranMueller <kieran98mueller@gmail.com>
2023-07-25 18:56:08 -0500

    create-announcement-overlay unfinished

--- frontend/src/app/components/create-announcement-overlay/create-announcement-overlay.component.ts
+++ frontend/src/app/components/create-announcement-overlay/create-announcement-overlay.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-announcement-overlay',
   templateUrl: './create-announcement-overlay.component.html',
-  styleUrls: ['./create-announcement-overlay.component.css']
+  styleUrls: ['./create-announcement-overlay.component.css'],
 })
 export class CreateAnnouncementOverlayComponent {
-
+  isOpen: boolean = true;
 }
