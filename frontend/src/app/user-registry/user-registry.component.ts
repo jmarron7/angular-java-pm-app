a204b115ef71065160fb660c6d24fca5c7ce9500
KieranMueller <kieran98mueller@gmail.com>
2023-07-25 10:28:05 -0500

    user registry started, need backend up to continue

--- frontend/src/app/user-registry/user-registry.component.ts
+++ frontend/src/app/user-registry/user-registry.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-registry',
   templateUrl: './user-registry.component.html',
-  styleUrls: ['./user-registry.component.css']
+  styleUrls: ['./user-registry.component.css'],
 })
-export class UserRegistryComponent {
-
-}
+export class UserRegistryComponent {}
