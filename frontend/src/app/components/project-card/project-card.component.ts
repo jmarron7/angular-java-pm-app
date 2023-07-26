0e1084f1b1311e4bbcf385889b9884969bb96a39
KieranMueller <kieran98mueller@gmail.com>
2023-07-26 15:13:36 -0500

    create-project-overlay in progress

--- frontend/src/app/components/project-card/project-card.component.ts
+++ frontend/src/app/components/project-card/project-card.component.ts
@@ -1,26 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProjectDto } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.css']
+  styleUrls: ['./project-card.component.css'],
 })
 export class ProjectCardComponent {
-
   @Input() project: ProjectDto = new ProjectDto();
+  modalVisible: boolean = false;
   daysAgo: number = 0;
+
+  @Output() toggleModal = new EventEmitter<any>();
 
   ngOnInit() {
     this.daysAgo = (() => {
-    let date1 = new Date(this.project.date);
-    let date2 = new Date();
-    
-    console.log(this.project.date)
-    console.log(date2)
-    let millDifference = date2.getTime() - date1.getTime();
-      
-    return Math.floor(millDifference / (1000 * 3600 * 24));
-    })()
+      let date1 = new Date(this.project.date);
+      let date2 = new Date();
+
+      console.log(this.project.date);
+      console.log(date2);
+      let millDifference = date2.getTime() - date1.getTime();
+
+      return Math.floor(millDifference / (1000 * 3600 * 24));
+    })();
+  }
+
+  edit() {
+    this.modalVisible = !this.modalVisible;
   }
 }
