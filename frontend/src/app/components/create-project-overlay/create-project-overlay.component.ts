0f8aad1d794c84c8d8a46931de03c823f13fd4c3
KieranMueller <kieran98mueller@gmail.com>
2023-07-26 12:23:09 -0500

    create project skeleton set up

--- frontend/src/app/components/create-project-overlay/create-project-overlay.component.ts
+++ frontend/src/app/components/create-project-overlay/create-project-overlay.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-project-overlay',
   templateUrl: './create-project-overlay.component.html',
-  styleUrls: ['./create-project-overlay.component.css']
+  styleUrls: ['./create-project-overlay.component.css'],
 })
-export class CreateProjectOverlayComponent {
+export class CreateProjectOverlayComponent implements OnInit {
+  isOpen: boolean = true;
+  projectName: string = '';
+  description: string = '';
+  success: boolean = false;
+  fail: boolean = false;
+  companyId: number = 0;
+  teamId: number = 0;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const companyId = JSON.parse(localStorage.getItem('companyId')!);
+    if (companyId) this.companyId = companyId;
+  }
+
+  createProject() {
+    this.http
+      .post(
+        `http://localhost:8080/company/${this.companyId}/teams/${this.teamId}/project`,
+        {
+          name: this.projectName,
+          description: this.description,
+          active: true,
+          team: {
+            id: 0,
+            name: 'test',
+            description: 'test',
+            teammates: [
+              {
+                id: 0,
+                profile: {
+                  firstName: 'test',
+                  lastName: 'test',
+                  email: 'test',
+                  phone: 'test',
+                },
+                admin: false,
+                active: true,
+                status: 'JOINED',
+              },
+            ],
+          },
+        }
+      )
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (e) => {
+          console.log(e);
+          this.fail = true;
+          setTimeout(() => {
+            this.isOpen = false;
+          }, 700);
+        },
+        complete: () => {
+          console.log('complete');
+          this.success = true;
+          setTimeout(() => {
+            this.isOpen = false;
+          }, 700);
+        },
+      });
+  }
 }
