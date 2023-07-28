d8d157641806c86df957a5c7fd0bef827ca2b7b2
KieranMueller <kieran98mueller@gmail.com>
2023-07-28 01:25:41 -0500

    ran ng update, updates packages

--- frontend/src/app/services/role.guard.ts
+++ frontend/src/app/services/role.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoleGuard implements CanActivate {
+export class RoleGuard  {
   constructor(private router: Router){}
   canActivate(
     next: ActivatedRouteSnapshot,
